0af495cb6852ae11a2e0a3dc36ec699235132235
Pavel <enthery@mail.ru>
2024-10-29 16:55:14 +0300

    Added context, navbar

--- src/App.js
+++ src/App.js
@@ -1,7 +1,23 @@
+import { Routes, Route } from "react-router-dom";
 import "./App.css";
+import NavBar from "./components/navbar";
+import Home from "./pages/home/index";
+import Details from "./pages/details/index";
+import Favorites from "./pages/favorites/index";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div>
+      <div className="min-h-screen p-6 bg-white text-gray-600 text-lg">
+        <NavBar />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/favorites" element={<Favorites />} />
+          <Route path="/recipe-item/:id" element={<Details />} />
+        </Routes>
+      </div>
+    </div>
+  );
 }
 
 export default App;
