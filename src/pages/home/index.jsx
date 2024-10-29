1d8fcb44f95244d8edb928babaa54e9da80dbaba
Pavel <enthery@mail.ru>
2024-10-29 18:29:00 +0300

    Added RecipeItem

--- src/pages/home/index.jsx
+++ src/pages/home/index.jsx
@@ -1,3 +1,24 @@
+import { useContext } from "react";
+import { GlobalContext } from "../../context/index";
+import RecipeItem from "../../components/recipe-item";
+
 export default function Home() {
-  return <div>Home</div>;
+  const { recipeList, loading } = useContext(GlobalContext);
+
+  if (loading) {
+    return <div>Loading...Please wait!</div>;
+  }
+  return (
+    <div className="py-8 container mx-auto flex-wrap flex justify-center gap-10">
+      {recipeList && recipeList.length > 0 ? (
+        recipeList.map((item) => <RecipeItem item={item} />)
+      ) : (
+        <div>
+          <p className="lg:text-4xl text-x1 text-center text-black font-extralight">
+            Nothing to show. Please seatch something
+          </p>
+        </div>
+      )}
+    </div>
+  );
 }
